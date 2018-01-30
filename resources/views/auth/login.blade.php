8e9c009dc869a04a742b78c2e6ac3b80208b461a
hallitee <hallitee_2005@yahoo.com>
2018-01-30 16:32:06 +0100

    after adding objectives file

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -54,7 +54,9 @@
                                 <button type="submit" class="btn btn-primary">
                                     Login
                                 </button>
-
+                                <a class="btn btn-link" href="{{ route('register') }}">
+                                    Register
+                                </a>
                                 <a class="btn btn-link" href="{{ route('password.request') }}">
                                     Forgot Your Password?
                                 </a>
